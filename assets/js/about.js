8700b5bebde3f5cba751b861fa9c03ab670ecc90
jhidai <kondodaviel@gmail.com>
2024-10-07 12:34:40 +0000

    preparation du nouveau repo pour déploiment

---
+++ assets/js/about.js
@@ -0,0 +1,65 @@
+// let pillars = document.getElementById("pillars");
+
+// let cardName = document.createElement("div");
+// cardName.setAttribute("class") = "card_name";
+
+// let cardIcon = document.createElement("div");
+// cardName.setAttribute("class") = "card_icon";
+
+// let icon = document.createElement("i")
+// cardName.setAttribute("class") = "";
+
+// let card = document.createElement("div")
+// cardName.setAttribute("class") = "card";
+
+// cardIcon.appendChild(icon);
+// card.appendChild(cardIcon);
+// card.appendChild(cardName);
+
+// pillars.appendChild(card);
+
+
+
+/* Handling the pillar modal toggling and filling it with the right data from dataJS */
+
+// getting modal container from HTML
+let card_modal = document.getElementById("pillar_modal");
+
+
+// setting the Closer variable for later use
+let closer 
+
+
+ //getting all the cards
+let cards = document.querySelectorAll(".card")
+
+cards.forEach(card  =>{
+    card.addEventListener("click", ()=>{
+        // getting the pillar name from the card through data attribute
+        let data = card.getAttribute("data")
+
+        // Showing the pillar modal
+        card_modal.style.display = "flex"
+
+        // getting the title and text container from the HTML
+        // adding the title and the body text to the modal
+        let title = document.getElementById("pillar_modal_title")
+        title.textContent = pillars[data][0]
+        let text = document.getElementById("pillar_modal_text")
+        
+        text.innerHTML = pillars[data][1]
+
+
+
+
+
+
+        // getting the closer tag from HTML
+        closer = document.getElementById("closer");
+
+        // Setting an event listener on the closer tag for open and close toggling
+        closer.addEventListener("click", () => {
+            card_modal.style.display = "none"
+        })
+    })
+})
---
+++ assets/js/data.js
@@ -0,0 +1,133 @@
+/* Website Data Container for dynamic layout to replaced by data fetched from the database on next versions */
+
+let pillars = {
+    "faith": ["faith",`'<i>For whatsoever is born of God overcometh the world: and this is the victory that overcometh the world, even our faith.'</i>
+    <br/><b>1 John 5:4</b><br/><br/>
+    <i>Above all, taking the shield of faith, wherewith ye shall be able to quench all the fiery darts of the wicked.'</i>
+    <br/><b>Ephesians 6:16</b><br/><br/>`],
+    
+"word": ["The Word",`'<i>“Who being the brightness of his glory, and the express image of his person, and upholding all things by the word of his power, when he had by himself purged our sins, sat down on the right hand of the Majesty on high.”</i>
+    <br/><b>Heb 1:3</b><br/><br/>
+    <br/><b>John 1:1-12</b><br/><br/>`],
+
+"supernatural": ["The Supernatural",`'<i>“The wind bloweth where it listeth, and thou hearest the sound thereof, but canst not tell whence it cometh, and whither it goeth: so is every one that is born of the Spirit.”</i>
+    <br/><b>1 John 3:8</b><br/><br/>
+    <br/><b>Ps 82:5-7</b><br/><br/>`],
+
+"praise": ["Praise",`'<i>“And when they began to sing and to praise, the Lord set ambushments against the children of Ammon, Moab, and mount Seir, which were come against Judah: and they were smitten.”</i>
+    <br/><b>2 Chro 20:22</b><br/><br/>
+    <br/><b>Psalm 67:1-7</b><br/><br/>
+    <br/><b>Psalm 149:1-9</b><br/><br/>`],
+
+"holy_spirit": ["The Holy Spirit",`'<i>“And it shall come to pass in that day, that his burden shall be taken away from off thy shoulder, and his yoke from off thy neck, and the yoke shall be destroyed because of the anointing.”</i>
+    <br/><b>Isaiah 10:27</b><br/><br/>
+    <br/><b>Acts 1:1-8</b><br/><br/>`],
+
+"prosperity": ["Prosperity",`'<i>“Beloved, I wish above all things that thou mayest prosper and be in health, even as thy soul prospereth.”</i>
+    <br/><b> 3 John 2</b><br/><br/>
+    <br/><b>Psalm 35:27</b><br/>
+    <br/><b>Zechariah 1:17</b><br/><br/>`],
+
+"vision": ["Vision",`'<i>“Where there is no vision, the people perish: but he that keepeth the law, happy is he.”</i>
+    <br/><b>Proverbs 29:18</b><br/><br/>
+    <br/><b> Jeremiah 29:11</b><br/><br/>`],
+
+"prayer": ["Prayer",`'<i>“And this is the confidence that we have in him, that, if we ask anything according to his will, he heareth us.”</i>
+    <br/><b>1 John 5:14</b><br/><br/>`
+],
+
+"healing": ["Healing",`'<i>“That it might be fulfilled which was spoken by Esaias the prophet, saying, Himself took our infirmities, and bare our sicknesses.”</i>
+    <br/><b>Matthew 8:17</b><br/><br/>
+    <br/><b>Jeremiah 8:22</b><br/>
+    <br/><b>Matthew 8:17</b><br/><br/>`],
+
+"wisdom": ["Wisdom",`'<i>“And wisdom and knowledge shall be the stability of thy times, and strength of salvation: the fear of the Lord is his treasure.”</i>
+    <br/><b> Isaiah 33:6</b><br/><br/>
+    <br/><b>Proverbs 24:3-4</b><br/><br/>`],
+
+"consecration": ["Consecration",`'<i>“Nevertheless the foundation of God standeth sure, having this seal, the Lord knoweth them that are his. And let every one that nameth the name of Christ depart from iniquity. “</i>
+    <br/><b>2 Timothy 2:19</b><br/><br/>
+    <br/><b>Hebrews 12:14</b><br/><br/>`],
+
+"success": ["Success",`'<i>“This book of the law shall not depart out of thy mouth; but thou shalt meditate therein day and night, that thou mayest observe to do according to all that is written therein: for then thou shalt make thy way prosperous, and then thou shalt have good success.”</i>
+    <br/><b>Jos 1:8</b><br/><br/>
+    `]
+
+}
+
+let events = {
+    "June_SFC": ["./assets/images/covenant_restoration.jpeg",
+    "Selebs For Celebs-June Edition",
+    `The Seasoned Leaders Breakthrough Summit For Celebrities(Selebs For Celebs) is a God ordained platform for the Lorem ipsum dolor 
+    sit amet consectetur, adipisicing elit. Id officia harum optio dolorem sint sapiente, vero voluptatum facilis praesentium rem blanditiis, nihil a rerum suscipit. Magni eveniet at soluta sed?`,
+    ["speakers",
+    "Pastor Michael CHUKWUDI (National Resident Pastor)",
+    "Pastor Eric ATIAMO (Assistant Resident Pastor)",
+    "Pastor Abel CHINATU (Associate Pastor)",
+    "Deaconess Cecilia Akintomide (Ordained Worker)",
+    "Deacon THOMPSON (Ordained Worker)"
+    ],
+    "28th June 2024",
+    "08.00 a.m. to 10.00 a.m",
+    "06/28/2024"
+    // ajouter ici un format de date que la classe date peut utiliser pour retourner unformat dde date utilisble pour la comparaison
+    /*
+    générer une date calculable ici avec la date texte précedente
+    calculuer la dufférence entre la date obtenue et la date du jour courant
+    stocker le résultat de la différence ici 
+    */
+],
+    "Mid_Year_Praise" : ["./assets/images/mid-year_praise_harvest.jpg",
+        "Mid-Year Praise Harvest Banquet",
+        "Themed <b>Overflow Praise</b> the Winners' 2024 Mid-Year Praise Banquet is a praise night to thank The Lord for his great works in The Winners' Family and in the life of every individual",
+        ["Starring",
+            "Chantre Ayawavi",
+            "Chantre Moïse",
+            "Chantre Florence Favour",
+            "Chantre Emilio Js",
+            "Chantre Elyze"
+        ],
+        "30th June 2024",
+        "From 4.00 p.m. (Red Carpet: 3.00 p.m.)",
+        "06/30/2024"
+    ]
+}
+
+let transports_checkpoints = {
+    "Attiégou":{
+        "DVA":"https://maps.app.goo.gl/mh2TS79fGpB49QfF9",
+        "EPP Attiégou": "https://maps.app.goo.gl/pywgV6SQFFjodvds7",
+        "DVA":"https://maps.app.goo.gl/mh2TS79fGpB49QfF9",
+        "EPP Attiégou": "https://maps.app.goo.gl/pywgV6SQFFjodvds7",
+        "DVA":"https://maps.app.goo.gl/mh2TS79fGpB49QfF9",
+        "EPP Attiégou": "https://maps.app.goo.gl/pywgV6SQFFjodvds7",
+        "DVA":"https://maps.app.goo.gl/mh2TS79fGpB49QfF9",
+        "EPP Attiégou": "https://maps.app.goo.gl/pywgV6SQFFjodvds7"
+    },
+    "Attiégou2":{
+        "DVA":"https://maps.app.goo.gl/mh2TS79fGpB49QfF9",
+        "EPP Attiégou": "https://maps.app.goo.gl/pywgV6SQFFjodvds7",
+        "DVA":"https://maps.app.goo.gl/mh2TS79fGpB49QfF9",
+        "EPP Attiégou": "https://maps.app.goo.gl/pywgV6SQFFjodvds7",
+        "DVA":"https://maps.app.goo.gl/mh2TS79fGpB49QfF9",
+        "EPP Attiégou": "https://maps.app.goo.gl/pywgV6SQFFjodvds7",
+        "DVA":"https://maps.app.goo.gl/mh2TS79fGpB49QfF9",
+        "EPP Attiégou": "https://maps.app.goo.gl/pywgV6SQFFjodvds7"
+    },
+    "Attiégou3":{
+        "DVA":"https://maps.app.goo.gl/mh2TS79fGpB49QfF9",
+        "EPP Attiégou": "https://maps.app.goo.gl/pywgV6SQFFjodvds7",
+        "DVA":"https://maps.app.goo.gl/mh2TS79fGpB49QfF9",
+        "EPP Attiégou": "https://maps.app.goo.gl/pywgV6SQFFjodvds7",
+        "DVA":"https://maps.app.goo.gl/mh2TS79fGpB49QfF9",
+        "EPP Attiégou": "https://maps.app.goo.gl/pywgV6SQFFjodvds7",
+        "DVA":"https://maps.app.goo.gl/mh2TS79fGpB49QfF9",
+        "EPP Attiégou": "https://maps.app.goo.gl/pywgV6SQFFjodvds7"
+    }
+}
+
+let giving_ways_data = {
+    "flooz" : ["./assets/images/flooz.png","Flooz Money",["<b>Offering:</b> 96 52 28 28","<b>Tithe:</b> 99 95 41 85"]],
+    "tmoney": ["./assets/images/tmoney.png","Tmoney",["<b>Offering:</b> 93 07 96 96","<b>Tithe:</b> 92 17 78 18"]],
+    "bank": ["./assets/images/bank_trans.png","Bank Transfer",["<b>Ecobank:</b> 1400 135 900 26"]]
+}
